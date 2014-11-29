e37b0c54524fd98d7a61e911cdabf251582930d4
Riady <riady_saz@yahoo.com>
2014-11-29 21:37:27 +0700

    halaman login

---
+++ src/name/bagi/levente/pedometer/Login.java
@@ -0,0 +1,43 @@
+package name.bagi.levente.pedometer;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class Login extends Activity {
+	private EditText textUsername;
+	private EditText textPassword;
+	private final String TAG = "Login";
+	private SharedPreferences mSettings;
+	public void onCreate(Bundle savedInstanceState){
+		Log.i(TAG, "[ACTIVITY] onCreate");
+		super.onCreate(savedInstanceState);
+
+		textUsername = (EditText) findViewById(R.id.userName); 
+		textPassword = (EditText) findViewById(R.id.password);
+	}
+
+	protected void onDestoy(){
+		Log.i(TAG, "[ACTIVITY] onDestroy");
+		super.onDestroy();
+
+	}
+	
+	public void onBackPressed(){
+		
+	}
+	
+	public void authen(){
+		String username,password;
+		username = textUsername.getText().toString();
+		password = textPassword.getText().toString();
+		Toast.makeText(this, "Isian harus diisi semua", Toast.LENGTH_SHORT).show();
+	}
+
+}
+
