f64c8487441dbba3f1fb91e329d826cc22297964
Riady <riady_saz@yahoo.com>
2014-11-30 01:44:41 +0700

    yang ini bisa

--- src/name/bagi/levente/pedometer/Login.java
+++ src/name/bagi/levente/pedometer/Login.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,12 +13,13 @@ import android.widget.Toast;
 public class Login extends Activity {
 	private EditText textUsername;
 	private EditText textPassword;
+	private Button loginButton;
 	private final String TAG = "Login";
 	private SharedPreferences mSettings;
 	public void onCreate(Bundle savedInstanceState){
 		Log.i(TAG, "[ACTIVITY] onCreate");
 		super.onCreate(savedInstanceState);
-
+		loginButton = (Button) findViewById(R.id.loginButton);
 		textUsername = (EditText) findViewById(R.id.userName); 
 		textPassword = (EditText) findViewById(R.id.password);
 	}
