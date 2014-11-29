8cce4fe3c99f24fa92d408f5584d36e240719dab
Riady <riady_saz@yahoo.com>
2014-11-30 00:12:38 +0700

    ngedit login dikit

--- src/name/bagi/levente/pedometer/Login.java
+++ src/name/bagi/levente/pedometer/Login.java
@@ -1,10 +1,12 @@
 package name.bagi.levente.pedometer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,12 +14,15 @@ import android.widget.Toast;
 public class Login extends Activity {
 	private EditText textUsername;
 	private EditText textPassword;
+	private Button loginButton;
 	private final String TAG = "Login";
 	private SharedPreferences mSettings;
 	public void onCreate(Bundle savedInstanceState){
 		Log.i(TAG, "[ACTIVITY] onCreate");
 		super.onCreate(savedInstanceState);
-
+		loginButton = (Button) findViewById(R.id.loginButton);
+		Intent myIntent = new Intent(Login.this, Comitment.class);
+		Login.this.startActivity(myIntent);
 		textUsername = (EditText) findViewById(R.id.userName); 
 		textPassword = (EditText) findViewById(R.id.password);
 	}
